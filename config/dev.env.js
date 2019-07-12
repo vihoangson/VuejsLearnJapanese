4adade4da00d4cb5c26fdd708fb8b5d8f79315be
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2019-07-12 16:52:04 +0700

    Set custom environment variable

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,10 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+const customEnv = require('./custom.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
 })
+
+module.exports = merge(prodEnv, customEnv)
