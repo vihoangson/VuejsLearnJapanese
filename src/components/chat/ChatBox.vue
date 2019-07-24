2b7e48026e857a50b76f89ee7c0b51719997a6f8
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2019-07-24 16:07:34 +0700

    Fix url

--- src/components/chat/ChatBox.vue
+++ src/components/chat/ChatBox.vue
@@ -645,7 +645,7 @@ textarea:focus:-webkit-placeholder {
 }
 .timeline-content-message pre {
     font-size: 14px;
-    font-family: initial;
+    font-family: arial;
     margin-top: 3px;
     margin-bottom: 0px;
 }
