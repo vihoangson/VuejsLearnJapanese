255734b5766b08762ba8cf3eb3942c474e417124
[Lampart] Hoang Sy Hung <sy_hung@lampart-vn.com>
2019-08-20 16:20:21 +0700

    update change room socket

--- src/services/socket/changeRoom.js
+++ src/services/socket/changeRoom.js
@@ -1,12 +1,12 @@
 export function changeRoom(store, room) {
     var listRoom = store.getters.get_list_room;
     switch (room.option) {
-        case 0:
+        case 0: // Add
             for (let i in listRoom) {
                 if (listRoom[i].room_id === room.room_id) {
-                    var member_list = listRoom[i].member_list;
-                    var member_update = [];
-                    var check_is_room = false;
+                    var memberList = listRoom[i].member_list;
+                    var memberUpdate = [];
+                    var isRoom = false;
                     for (let key in room.selected) {
                         if (
                             room.selected[key] !== null &&
@@ -16,37 +16,37 @@ export function changeRoom(store, room) {
                                 room.selected[key].id ===
                                 store.getters.get_current_user_info.id
                             ) {
-                                check_is_room = true;
+                                isRoom = true;
                             }
                         }
                     }
-                    if (check_is_room) {
-                        for (let key in member_list) {
+                    if (isRoom) {
+                        for (let key in memberList) {
                             for (let i in room.selected) {
                                 if (
                                     room.selected[i] !== null &&
                                     room.selected[i] !== 'empty'
                                 ) {
                                     if (
-                                        member_list[key].id ===
+                                        memberList[key].id ===
                                         room.selected[i].id
                                     ) {
-                                        member_list[key].role_in_room =
+                                        memberList[key].role_in_room =
                                             room.selected[i].permission;
-                                        member_update.push(member_list[key]);
+                                        memberUpdate.push(memberList[key]);
                                     }
                                 }
                             }
                         }
 
-                        listRoom[i].member_list = member_update;
+                        listRoom[i].member_list = memberUpdate;
                     } else {
                         listRoom.splice(i, 1);
                     }
                 }
             }
             break;
-        case 1:
+        case 1: // Update
             for (let i in listRoom) {
                 if (listRoom[i].room_id === room.room_id) {
                     var userAdd = [];
@@ -80,7 +80,7 @@ export function changeRoom(store, room) {
                 }
             }
             break;
-        case 2:
+        case 2: // Leave
             for (let i in listRoom) {
                 if (listRoom[i].room_id === room.room_id) {
                     if (listRoom[i].member_list !== undefined) {
